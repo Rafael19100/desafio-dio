458e17622886a19c4a141f7df95a6fa4cd6d8be3
Rafael19100 <raffael.a.nascimento3107@gmail.com>
2025-11-15 03:16:29 -0300

    meu segundo comit

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-let nome  = "Rafael";
+let vitorias = 230;
+let derrotas = 10;
 
-console.log("Tem que estudar " + nome);
+
+let saldoVitorias = vitorias - derrotas;
+
+
+let nivel = "";
+
+
+if (saldoVitorias <= 10) {
+    nivel = "Ferro";
+} 
+else if (saldoVitorias <= 20) {
+	nivel = "Bronze";
+} 
+else if (saldoVitorias <= 50) {
+    nivel = "Prata";
+} 
+else if (saldoVitorias <= 80) {
+    nivel = "Ouro";
+} 
+else if (saldoVitorias <= 90) {
+    nivel = "Diamante";
+} 
+else if (saldoVitorias <= 100) {
+    nivel = "Lendário";
+} 
+else {
+   
+    nivel = "Imortal";
+}
+
+
+console.log("O Herói tem saldo de " + saldoVitorias + " e está no nível " + nivel );
 
 
 
